358bc1d7cf8500eefdcf63f18a82ea13d20f104b
YoHaiYo <12si47bun@naver.com>
2024-01-14 11:37:21 +0900

    logo 추가 seo 수정

--- src/Section/Header.js
+++ src/Section/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Header() {
   return (
     <header>
-      <h1><a href='/'>Text to Png Magician</a></h1>
+      <h1>
+        <a href='/'>
+        <img src="./img/logo-pc.png" alt=""/>
+        {/* Text to Png Magician */}
+        </a>
+      </h1>
       <ul>
         <li>Abous Us</li>
         <li>Request Us</li>        
