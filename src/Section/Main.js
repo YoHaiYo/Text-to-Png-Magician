478fae135dc47d42fb46169c3efb26dde51b79ba
YoHaiYo <12si47bun@naver.com>
2024-02-03 10:47:13 +0900

    랜덤 스타일메지션 추가

--- src/Section/Main.js
+++ src/Section/Main.js
@@ -12,6 +12,8 @@ const Main = () => {
   const [textSize, setTextSize] = useState(50);
   const [textFamily, setTextFamily] = useState('sans-serif');
   const [isCopy, setIsCopy] = useState(false);
+  const [isShaking, setIsShaking] = useState(false); // 흔들림 상태 관리
+
 
   const ref = useRef(null);
 
@@ -52,6 +54,23 @@ const Main = () => {
     }
   };
 
+   // 스타일 랜덤 적용 함수
+   const applyRandomStyles = () => {
+    const randomColor = colordata[Math.floor(Math.random() * colordata.length)].color;
+    const randomSize = Math.floor(Math.random() * 96) + 25; // 25에서 120 사이의 크기 랜덤 선택
+    const randomFontFamily = fontdata[Math.floor(Math.random() * fontdata.length)].fontfamily;
+
+    setTextColor(randomColor);
+    setTextSize(randomSize);
+    setTextFamily(randomFontFamily);
+
+     // 아이콘을 흔들도록 클래스 추가 및 제거
+     setIsShaking(true);
+     setTimeout(() => {
+       setIsShaking(false);
+     }, 1250); // 1초 후 흔들림 효과 제거
+  };
+
   const saveImageHandler = () => {
     if (ref.current) {
       const saveImageStyle = {
@@ -81,19 +100,23 @@ const Main = () => {
 
   const handleTextChange = (event) => {
     setTextContent(event.target.value);
+    
   };
 
   const handleColorChange = (event) => {
     setTextColor(event.target.value);
+    
   };
 
   const handleSizeChange = (event) => {
-    const newSize = parseInt(event.target.value, 10);
-    setTextSize(isNaN(newSize) ? 16 : newSize);
+    // const newSize = parseInt(event.target.value, 10);
+    setTextSize(event.target.value);
+   
   };
 
   const handleFontChange = (family) => {
     setTextFamily(family);
+  
   };
 
   return (
@@ -121,7 +144,8 @@ const Main = () => {
         <div className='d-lg-flex align-items-center '>
           <label className='mybasic myoption ' htmlFor="textSize">Size</label>
           <div>
-            <button className="mybasic mybtn__white " onClick={() => setTextSize(25)}>25px</button>
+            <button className={`mybasic mybtn__white`} 
+            onClick={() => {setTextSize(25);}}>25px</button>
             <button className="mybasic mybtn__white " onClick={() => setTextSize(50)}>50px</button>
             <button className="mybasic mybtn__white " onClick={() => setTextSize(75)}>75px</button>
             <button className="mybasic mybtn__white " onClick={() => setTextSize(100)}>100px</button>
@@ -133,9 +157,9 @@ const Main = () => {
 
         {/* font family btns */}
         <div className='d-lg-flex align-items-center'>
-          <label className='mybasic myoption'>Font</label>      
           {/* `ssh 얘는 넣을게 많으니 배열로 빼서 map으로 처리하기 */}
-          <div>
+          <div className='fontwrap'>
+          <label className='mybasic myoption '>Font</label>      
             {
               fontdata.map((el, idx)=> {
                 return (
@@ -147,6 +171,20 @@ const Main = () => {
               })
             }
           </div>
+        </div>
+
+        {/* magician btns */}
+        <div className='d-lg-flex justify-content-end align-items-center'>
+          <img className={`${isShaking ? 'opacity-100 magician_twinkle-down' : 'opacity-0'}`} src='./img/twinkle.png' style={{ width: 30}} // 클릭시 반짝 효과
+          /> 
+          <button className={`mybasic mybtn__puple text-white ${isShaking ? 'magician_shake' : ''}`} // 흔들림 클래스 추가
+           onClick={() => applyRandomStyles()}>
+          <img src='./img/Text_to_Png_Magician_icon.png' style={{ width: 30}}/>
+          Style Magician          
+          </button>
+          <img className={`${isShaking ? 'opacity-100 magician_twinkle-up' : 'opacity-0'}`} src='./img/twinkle.png' style={{ width: 30}} // 클릭시 반짝 효과
+          /> 
+          
         </div>
 
        {/* input output box */}
@@ -175,14 +213,14 @@ const Main = () => {
 
         {/* copy buttons */}
         <div className='text-align-end d-lg-flex justify-content-center justify-content-lg-end mt-2'> 
-          <button className='mybasic mybtn__red' 
+          <button className='mybasic mybtn__red fw-bold' 
           onClick={()=>{
               clipboardHandler()
               setIsCopy(true)
             }}>
           Copy to PNG<i className={`mx-2 ${isCopy ? "bi bi-check-lg" : "bi bi-copy"}`}></i></button>
           
-          <button className='mybasic mybtn__red'  onClick={saveImageHandler}>
+          <button className='mybasic mybtn__red fw-bold '  onClick={saveImageHandler}>
             Save to PNG<i className="bi bi-printer mx-2"></i></button>
         </div>
       </section>
